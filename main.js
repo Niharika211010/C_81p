d8aabba103caea084963cbab8fe89aa686aecc4d
Niharika211010 <130831841+Niharika211010@users.noreply.github.com>
2023-10-03 19:18:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 1;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.strokeStyle();
